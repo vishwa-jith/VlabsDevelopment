bd06074d6f3496883d52a6571b6f2c31fe89a497
vishwa-jith <vishwajith567@gmail.com>
2021-03-13 14:46:38 +0530

    add demodulation

---
+++ AMPLITUDE MODULATION/js/demodulation.js
@@ -0,0 +1,230 @@
+let oscilloscopeCanvas1 = document.querySelector("#oscilloscope-canvas1");
+let oscilloscopeCanvas2 = document.querySelector("#oscilloscope-canvas2");
+let oscilloscopeCanvas3 = document.querySelector("#oscilloscope-canvas3");
+let canvas = document.querySelectorAll("canvas");
+let selectWave = document.querySelector("#selectWave");
+let selectedWave = document.querySelector("#selectedWave");
+let amplitudeMessage = document.querySelector("#amplitudeMessage");
+let frequencyMessage = document.querySelector("#frequencyMessage");
+let oscilloscope = document.querySelector("#oscilloscope");
+let spectrum = document.querySelector("#spectrum");
+let graphRep = document.querySelector("#graphRep");
+let heading = document.querySelector("#heading");
+let oscilloscopeCanvas = document.querySelector("#oscilloscope-canvas");
+
+let darkCyan = "#00796b";
+let lightCyan = "#b2dfdb";
+let light = "#eee";
+var amYPos1 = [],
+  amYPos2 = [],
+  amYPos3 = [],
+  t = 0;
+let graphType = "Oscilloscope";
+const PI = Math.PI;
+let context1 = oscilloscopeCanvas1.getContext("2d");
+let context2 = oscilloscopeCanvas2.getContext("2d");
+let context3 = oscilloscopeCanvas3.getContext("2d");
+let currentCanvas = null;
+
+let parameters = [
+  {
+    amArr: amYPos1,
+    context: context1,
+    messageAmp: 10,
+    messageFreq: 20,
+    carrierAmp: 40,
+    carrierFreq: 100,
+  },
+  {
+    amArr: amYPos2,
+    context: context2,
+    messageAmp: 30,
+    messageFreq: 20,
+    carrierAmp: 30,
+    carrierFreq: 100,
+  },
+  {
+    amArr: amYPos3,
+    context: context3,
+    messageAmp: 43,
+    messageFreq: 20,
+    carrierAmp: 7,
+    carrierFreq: 100,
+  },
+];
+const drawSignals = (
+  context,
+  t,
+  amYPos,
+  messageAmp,
+  messageFreq,
+  carrierAmp,
+  carrierFreq
+) => {
+  displaySignalLabel(context);
+  drawAmplitudeModulationSignal(
+    context,
+    t,
+    amYPos,
+    oscilloscopeCanvas1.height / 2,
+    messageAmp,
+    messageFreq,
+    carrierAmp,
+    carrierFreq
+  );
+};
+function displaySignalLabel(context) {
+  context.beginPath();
+  context.fillStyle = darkCyan;
+  context.font = "16px Arial";
+  context.fillText("am(t)", 10, 20);
+  context.closePath();
+}
+const drawAmplitudeModulationSignal = (
+  context,
+  t,
+  amArr,
+  yOffset,
+  messageAmp,
+  messageFreq,
+  carrierAmp,
+  carrierFreq
+) => {
+  let tempMessageValue = messageAmp * Math.cos(2 * PI * messageFreq * t);
+  let y =
+    (parseInt(carrierAmp) + tempMessageValue) *
+    Math.cos(2 * PI * carrierFreq * t);
+
+  amArr.unshift(y);
+  for (let i = 0; i < amArr.length; i++) {
+    context.beginPath();
+    context.fillStyle = darkCyan;
+    context.arc(i, yOffset - amArr[i], 2, 0, 2 * PI);
+    context.fill();
+    context.closePath();
+    if (amArr.length > oscilloscopeCanvas1.width) {
+      amArr.pop();
+    }
+  }
+};
+
+const drawMessageSignal = (t) => {
+  drawSignal(
+    messageAmp,
+    messageFreq,
+    t,
+    messageYPos,
+    oscilloscopeCanvas.height / 3 / 2
+  );
+};
+
+function loop() {
+  context1.clearRect(
+    0,
+    0,
+    oscilloscopeCanvas1.width,
+    oscilloscopeCanvas1.height
+  );
+  context2.clearRect(
+    0,
+    0,
+    oscilloscopeCanvas1.width,
+    oscilloscopeCanvas1.height
+  );
+  context3.clearRect(
+    0,
+    0,
+    oscilloscopeCanvas1.width,
+    oscilloscopeCanvas1.height
+  );
+  t += PI / 180 / 100;
+  for (var i = 0; i < parameters.length; i++) {
+    drawSignals(
+      parameters[i].context,
+      t,
+      parameters[i].amArr,
+      parameters[i].messageAmp,
+      parameters[i].messageFreq,
+      parameters[i].carrierAmp,
+      parameters[i].carrierFreq
+    );
+  }
+  requestAnimationFrame(loop);
+}
+loop();
+
+canvas.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    currentCanvas = event.target.id.slice(-1);
+    selectWave.classList.add("d-none");
+    selectedWave.classList.remove("d-none");
+    amplitudeMessage.value = parameters[currentCanvas - 1].messageAmp;
+    frequencyMessage.value = parameters[currentCanvas - 1].messageFreq;
+
+    let context = oscilloscopeCanvas.getContext("2d");
+    var messageYPos = [],
+      t = 0;
+    const PI = Math.PI;
+    const drawSignals = (t) => {
+      displaySignalLabel();
+      drawMessageSignal(t);
+    };
+    function displaySignalLabel() {
+      context.beginPath();
+      context.fillStyle = darkCyan;
+      context.font = "16px Arial";
+      context.fillText("m(t)", 10, 20);
+      context.closePath();
+    }
+    const drawMessageSignal = (t) => {
+      drawSignal(
+        parameters[currentCanvas - 1].messageAmp,
+        parameters[currentCanvas - 1].messageFreq,
+        t,
+        messageYPos,
+        oscilloscopeCanvas.height / 2
+      );
+    };
+
+    const drawSignal = (amplitude, frequency, t, arr, yOffset) => {
+      let y = amplitude * Math.cos(2 * PI * frequency * t);
+      arr.unshift(y);
+      for (let i = 0; i < arr.length; i++) {
+        context.beginPath();
+        context.fillStyle = darkCyan;
+        context.arc(i, yOffset - arr[i], 2, 0, 2 * PI);
+        context.fill();
+        context.closePath();
+        if (arr.length > oscilloscopeCanvas.width) {
+          arr.pop();
+        }
+      }
+    };
+    function loop() {
+      context.clearRect(
+        0,
+        0,
+        oscilloscopeCanvas.width,
+        oscilloscopeCanvas.height
+      );
+      t += PI / 180 / 100;
+      drawSignals(t);
+      requestAnimationFrame(loop);
+    }
+    loop();
+  });
+});
+
+graphRep.addEventListener("click", () => {
+  tempRep = graphRep.innerHTML;
+  graphRep.innerHTML = graphType;
+  graphType = tempRep;
+  if (graphType === "Oscilloscope") {
+    oscilloscopeCanvas.classList.remove("d-none");
+    spectrum.classList.add("d-none");
+  } else {
+    spectrum.classList.remove("d-none");
+    oscilloscopeCanvas.classList.add("d-none");
+  }
+  heading.innerHTML = graphType;
+});
