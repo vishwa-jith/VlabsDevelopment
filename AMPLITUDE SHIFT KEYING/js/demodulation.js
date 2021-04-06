e56c4346bd90c7e335e230073157abc9de7c6668
vishwa-jith <vishwajith567@gmail.com>
2021-04-06 23:36:39 +0530

    demodulation setup

---
+++ AMPLITUDE SHIFT KEYING/js/demodulation.js
@@ -0,0 +1,3 @@
+let oscilloscopeCanvas1 = document.querySelector("#oscilloscope-canvas1");
+let oscilloscopeCanvas2 = document.querySelector("#oscilloscope-canvas2");
+let oscilloscopeCanvas3 = document.querySelector("#oscilloscope-canvas3");
